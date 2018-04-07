41f52d441beb17a86380686c67ddf364df60f287
Koichi Nakanishi <keizo042dev@gmail.com>
2018-04-08 00:43:04 +0900

    Implement basic impl

--- gopaths.go
+++ gopaths.go
@@ -8,19 +8,47 @@ import (
 	"strings"
 )
 
+var (
+	GOPATHS_ENV_ORIGINAL_GOPATH = "GOPATHS_GOPATH"
+	ERR_NOTIMPL                 = errors.New("NotImplemented")
+)
+
 type (
 	App struct {
-		GOPath string
-		Info   *RepoInfo
+		GOPATH    string
+		ReposPath string
+		Info      *RepoInfo
+	}
+
+	AppInitConfig struct {
+	}
+
+	AppConfigConfig struct {
+	}
+
+	AppAddConfig struct {
+		Path string
+	}
+
+	AppRemoveConfig struct {
+		Path string
+	}
+
+	AppEnableConfig struct {
+	}
+
+	AppDisableConfig struct {
 	}
 
 	Config struct {
-		configPath string
-		reposPath  string
+		reposPath string
 	}
 
 	RepoInfo struct {
-		Repos []string
+		Version      int32
+		GOPATH       string
+		Repos        []string
+		DisableRepos []string
 	}
 )
 
@@ -32,42 +60,87 @@ func getInfo(fpath string) (*RepoInfo, error) {
 	return &rinfo, nil
 }
 
+func setInfo(fpath string, rinfo *RepoInfo) error {
+	f, err := os.Open(fpath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return toml.NewEncoder(f).Encode(rinfo)
+}
+
 func NewApp(c *Config) (*App, error) {
 	inf, err := getInfo(c.reposPath)
 	if err != nil {
 		return nil, errors.Wrap(err, "repos info")
 	}
 	return &App{
-		GOPath: os.Getenv("GOPATH"),
+		GOPATH: os.Getenv("GOPATH"),
 		Info:   inf,
 	}, nil
 }
 
+// Init is `gopaths init`.
+// for initalizing GOPATH which gopaths maintain.
 func (app *App) Init() error {
 	path := strings.Join(app.Info.Repos, ":")
-	return fmt.Printf("export GOPATH=$GOPATH:%s", path)
-}
-
-func (app *App) Config() error {
+	if err := os.Setenv(GOPATHS_ENV_ORIGINAL_GOPATH, path); err != nil {
+		return err
+	}
+	if _, err := fmt.Printf("export GOPATH=$GOPATH:%s", path); err != nil {
+		return err
+	}
 	return nil
 }
 
+// Config is `gopaths config`
+// for config gopaths configuration.
+func (app *App) Config() error {
+	return ERR_NOTIMPL
+}
+
+// Enable is `gopaths enable`.
+//
 func (app *App) Enable() error {
-	return errors.New("TBD")
+	paths := strings.Join(app.Info.Repos, ":")
+	return os.Setenv("GOPATH", app.Info.GOPATH+":"+paths)
 }
 
-func (app *App) Disable() error {
-	return errors.New("TBD")
+// Disable is `gopaths disable`.
+func (app *App) Disable(config AppDisableConfig) error {
+	return os.Setenv("GOPATH", app.Info.GOPATH)
 }
 
-func (app *App) Add() error {
-	return errors.New("TBD")
+// Add is `gopaths add`.
+//
+func (app *App) Add(config AppAddConfig) error {
+	rinfo, err := getInfo(app.ReposPath)
+	if err != nil {
+		return err
+	}
+	rinfo.Repos = append(rinfo.Repos, config.Path)
+	return setInfo(app.ReposPath, rinfo)
 }
 
-func (app *App) Remove() error {
-	return errors.New("TBD")
+// Remove is `gopaths remove`
+//
+func (app *App) Remove(config AppRemoveConfig) error {
+	var repos []string
+	for _, path := range app.Info.Repos {
+		if strings.Compare(path, config.Path) != 0 {
+			repos = append(repos, path)
+		}
+	}
+	return setInfo(app.ReposPath, app.Info)
 }
 
+// Complete is `gopaths complete`.
+//
 func (app *App) Complete() error {
-	return errors.New("TBD")
+	return ERR_NOTIMPL
+}
+
+// Clean is `gopaths clean`.
+func (app *App) Clean() error {
+	return ERR_NOTIMPL
 }
