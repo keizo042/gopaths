96f8c2f8b84e28169889a5c846bc46b8c2f4d41c
Koichi Nakanishi <keizo042dev@gmail.com>
2018-04-08 00:36:34 +0900

    Add init&clean test

--- gopaths_test.go
+++ gopaths_test.go
@@ -1,9 +1,23 @@
 package gopaths
 
 import (
-	"github.com/pkg/errors"
+	"os"
 	"testing"
 )
 
 func TestApp(t *testing.T) {
+	testApp := &App{
+		GOPATH:    "",
+		Info:      &RepoInfo{},
+		ReposPath: "",
+	}
+	if err := testApp.Init(); err != nil {
+		t.Errorf("`gopaths init` : %v", err)
+	}
+	if err := testApp.Clean(); err != nil {
+		t.Errorf("`gopaths clean`: %v", err)
+	}
+	if v := os.Getenv(GOPATHS_ENV_ORIGINAL_GOPATH); v != "" {
+		t.Errorf("%s is not clean: %s", GOPATHS_ENV_ORIGINAL_GOPATH, v)
+	}
 }
